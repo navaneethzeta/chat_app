c004bd39ea48bf8c7ae31f6f4c22e2e48cceb9dc
m s dhoni <navaneethpa13@gmail.com>
2021-08-29 15:43:13 +0530

    chat app done

--- lib/screens/welcome/messages/components/body.dart
+++ lib/screens/welcome/messages/components/body.dart
@@ -5,6 +5,8 @@ import 'package:chat_app/models/ChatMessage.dart';
 import 'package:flutter/material.dart';
 
 import 'chat_input_field.dart';
+import 'message.dart';
+
 
 class Body  extends StatelessWidget {
 
@@ -26,45 +28,6 @@ class Body  extends StatelessWidget {
         ChatInputField()
       ],
     );
-  }
-}
-
-class Message extends StatelessWidget {
-  const Message({
-    Key key,
-    @required this.message,
-  }) : super(key: key);
-     final ChatMessage message;
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: message.isSender ? MainAxisAlignment.end : MainAxisAlignment.start,
-      children: [
-        TextMessage(message: message),
-      ],
-    );
-  }
-}
-
-class TextMessage extends StatelessWidget {
-  const TextMessage({
-    Key key, this.message,
-  }) : super(key: key);
- final ChatMessage message;
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: kDefaultPadding),
-      padding: EdgeInsets.symmetric(horizontal: kDefaultPadding* 0.75,
-       vertical: kDefaultPadding/2),
-       decoration: BoxDecoration(color: kPrimaryColor.withOpacity(message.isSender ? 1: 0.8), borderRadius: BorderRadius.circular(30),
-       ),
-      child: Text(
-        message.text, 
-        style: TextStyle(color: message.isSender ? Colors.white : Theme.of(context).textTheme.bodyText1.color,
-         ),
-        ),
-      );
   }
 }
 
